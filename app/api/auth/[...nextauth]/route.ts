07dec433514b73550c713ec0cb5b66872a177986
CarlosGPC <carlosgahelpulidocervantes@gmail.com>
2024-08-21 23:26:16 -0700

    Corregidas las funciones HTTP (GET, POST) directamente

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,11 +1,11 @@
-import NextAuth, { AuthOptions } from 'next-auth';
+import NextAuth from 'next-auth';
 import CredentialsProvider from 'next-auth/providers/credentials';
 import { PrismaClient } from '@prisma/client';
 import bcrypt from 'bcryptjs';
 
 const prisma = new PrismaClient();
 
-export const authOptions: AuthOptions = {
+const authOptions = {
     providers: [
         CredentialsProvider({
             name: 'Credentials',
@@ -14,35 +14,31 @@ export const authOptions: AuthOptions = {
                 password: { label: 'Password', type: 'password' },
             },
             async authorize(credentials) {
-                // Encuentra al usuario por email
                 const user = await prisma.user.findUnique({
                     where: { email: credentials?.email },
                 });
 
-                // Verifica la contraseña
                 if (user && bcrypt.compareSync(credentials!.password, user.password)) {
-                    // Retorna el objeto de usuario que NextAuth espera
                     return {
-                        id: user.id.toString(),  // Convierte el id a string
+                        id: user.id.toString(),
                         email: user.email,
                         name: user.name,
                         image: user.image,
                     };
                 }
 
-                // Retorna null si no se encuentra el usuario o la contraseña es incorrecta
                 return null;
             },
         }),
     ],
     callbacks: {
-        async jwt({ token, user }) {
+        async jwt({ token, user }: { token: any, user: any }) {
             if (user) {
                 token.id = user.id;
             }
             return token;
         },
-        async session({ session, token }) {
+        async session({ session, token }: { session: any, token: any }) {
             if (token) {
                 session.user.id = token.id;
             }
